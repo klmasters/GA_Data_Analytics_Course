b7be56ca5698671053baf76ce4afeb1af16138a1
klmasters <45240461+klmasters@users.noreply.github.com>
2018-11-29 14:19:03 -0500

    Update class_09.sql

--- in_class_work/class_09.sql
+++ in_class_work/class_09.sql
@@ -1,64 +1,78 @@
-/** number 1 which products have a case cost of more than 100 **/ 
+--Which products have a case cost of more than $100?
 
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
     case_cost > 100
 ;
 
-/** number 2 which tequilas have a case cost of more than 100 **/
+-- Which tequilas have a case cost of more than $100?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
     category_name ilike '%tequila%'
     AND case_cost > 100
 ;
-/** number 3 which tequilas or scotch whiskies have a case cost of more than 100**/
+
+-- Which tequilas or scotch whiskies have a case cost of more than $100?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
     (category_name ILIKE '%tequila%' 
-    or
+    OR
     category_name ILIKE '%SCOTCH WHISKIES%')
     AND case_cost > 100
 ;
 
-/** number 4 which tequilas or scotch whiskies have a case cost between 100 and 120**/
+-- Which tequilas or scotch whiskies have a case cost between $100 and $120?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
     (category_name ILIKE '%tequila%' 
-    or
+    OR
     category_name ILIKE '%SCOTCH WHISKIES%')
     AND case_cost BETWEEN 100 AND 120
 ;
 
-/** number 5 which whiskies of any kind cost more than 100**/
+-- Which whiskies of any kind cost more than $100?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
-    category_name ilike '%whisk%'
+    category_name ILIKE '%whisk%'
     AND case_cost > 100
 ;
 
-/** number 6 **/
+-- Which whiskies of any kind cost between $100 and $150?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
-    category_name ilike '%whisk%'
+    category_name ILIKE '%whisk%'
     AND case_cost BETWEEN 100 AND 150
 ;
 
-/** number 7 **/
+-- Which products, excluding tequilas, cost between $100 and $120?
+
 SELECT
     *
-FROM products
+FROM 
+   products
 WHERE
     category_name NOT LIKE '%tequila%' 
     AND case_cost BETWEEN 100 AND 120
